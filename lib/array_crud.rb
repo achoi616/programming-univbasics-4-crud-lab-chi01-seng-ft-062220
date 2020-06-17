54ec14787938c3b02f1ce73d662f83a08e108e09
achoi616 <achoi616@gmail.com>
2020-06-17 05:06:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-
+  move = ["run", "squat"]
+  fit = move.pop
 end
 
 def remove_element_from_start_of_array(array)
