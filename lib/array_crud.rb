addbe3d1cb564360fa02f8ce6812253c7532738d
achoi616 <achoi616@gmail.com>
2020-06-17 04:54:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   ["squat", "push", "pull", "run"]
-  hinge.push
+  add_element_to_end_of_array << "hinge"
 end
 
 def add_element_to_start_of_array(array, element)
